68182cd9f6f01fd862218009543aba17a99cead4
Vladimir <woaouh@outlook.com>
2021-01-02 19:11:35 +0200

    Removed old redux code

--- src/redux/old-redux/actions/filters.js
+++
@@ -1,9 +0,0 @@
-export const setSortBy = (type) => ({
-  type: 'SET_SORT_BY',
-  payload: type,
-});
-
-export const setCategory = (category) => ({
-  type: 'SET_CATEGORY',
-  payload: category,
-});
