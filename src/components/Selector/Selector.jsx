6bd10e80c80fbadedfb37fe49e20c67300205bbe
Vladimir <woaouh@outlook.com>
2021-01-02 19:46:37 +0200

    Refactored Selector component.

--- src/components/Selector/Selector.jsx
+++ src/components/Selector/Selector.jsx
@@ -5,52 +5,36 @@ import classNames from 'classnames';
 import classes from './Selector.module.sass';
 
 export default function Selector({
-  types,
-  sizes,
-  activeType,
-  activeSize,
-  onSelectType,
-  onSelectSize,
+  types, sizes, activeType, activeSize, onSelectType, onSelectSize,
 }) {
   const doughTypes = ['thin', 'traditional'];
   const pizzaSizes = [26, 30, 40];
 
+  function renderOptions(option, activeKind, kinds, handler) {
+    return option.map((type, index) => (
+      <div
+        key={type}
+        className={classNames({
+          [classes.Active]: activeKind === index,
+          [classes.Disabled]: !kinds.includes(option === doughTypes ? index : type),
+        })}
+        onClick={() => handler(index)}
+        onKeyUp={() => handler(index)}
+        role="button"
+        tabIndex="0"
+      >
+        {type}
+      </div>
+    ));
+  }
+
   return (
     <div className={classes.Selector}>
       <div className={classes.SelectorList}>
-        {doughTypes.map((type, index) => (
-          <div
-            key={type}
-            className={classNames({
-              [classes.Active]: activeType === index,
-              [classes.Disabled]: !types.includes(index),
-            })}
-            onClick={() => onSelectType(index)}
-            onKeyUp={() => onSelectType(index)}
-            role="button"
-            tabIndex="0"
-          >
-            {type}
-          </div>
-        ))}
+        {renderOptions(doughTypes, activeType, types, onSelectType)}
       </div>
       <div className={classes.SelectorList}>
-        {pizzaSizes.map((size, index) => (
-          <div
-            key={size}
-            className={classNames({
-              [classes.Active]: activeSize === index,
-              [classes.Disabled]: !sizes.includes(size),
-            })}
-            onClick={() => onSelectSize(index)}
-            onKeyUp={() => onSelectSize(index)}
-            role="button"
-            tabIndex="0"
-          >
-            {size}
-            sm.
-          </div>
-        ))}
+        {renderOptions(pizzaSizes, activeSize, sizes, onSelectSize)}
       </div>
     </div>
   );
